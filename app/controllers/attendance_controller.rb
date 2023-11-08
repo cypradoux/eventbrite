9cbf9d96ddeb06782a845fd5631fb04dff9c0bc0
cypradoux <pradouxcyprien@gmail.com>
2023-11-08 18:25:51 +0100

    huit

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -1,18 +1,33 @@
 class AttendanceController < ApplicationController
     def create
-        @attendances = Attendance.new(stripe_customer_id: params[:stripe_customer_id])
-                    
-        # @events.user_id = current_user.id
 
-            if @attendances.save
-            flash[:success] = "Tu es bien inscrit à l'évènement !"
-            redirect_to event_index_path
-            else
-            flash[:alert] = "Une erreur s'est glissée, essaye à nouveau!"
-            redirect_to new_attendance_path
-            end 
+    end
+    
+    def new
+        @total = params[:total].to_d
+        @session = Stripe::Checkout::Session.create(
+          payment_method_types: ['card'],
+          line_items: [
+            {
+              price_data: {
+                currency: 'eur',
+                unit_amount: (@total*100).to_i,
+                product_data: {
+                  name: 'Rails Stripe Checkout',
+                },
+              },
+              quantity: 1
+            },
+          ],
+          mode: 'payment',
+          success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
+          cancel_url: checkout_cancel_url
+        )
+        redirect_to @session.url, allow_other_host: true
     end
 
-    def new
+    def success
+        @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+        @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
     end
 end
